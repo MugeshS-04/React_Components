92d301a655cb06889f514fd426739666b90fca9f
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-04-27 19:31:42 +0530

    Implemented useEffect for theme

--- React_Navbar/Navbar/src/App.jsx
+++ React_Navbar/Navbar/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 
 
 function App() {
+
+  const current_theme = localStorage.getItem('current_theme')
   
-  const [theme, settheme] = useState("light")
+  const [theme, settheme] = useState(current_theme? current_theme : "light")
+  
+  useEffect(() => {
+      localStorage.setItem('current_theme', theme)
+  },[theme])
   return (
     <>
       <div className={`container ${theme}`}>
