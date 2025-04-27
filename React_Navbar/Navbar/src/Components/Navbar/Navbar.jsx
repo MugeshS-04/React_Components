339e561ab264374c8e35ba26f9aef4d2cf44863b
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-04-27 18:36:47 +0530

    defined the structure of navbar

--- React_Navbar/Navbar/src/Components/Navbar/Navbar.jsx
+++ React_Navbar/Navbar/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './Navbar.css'
+import logo_light from '../../assets/logo-black.png'
+import logo_dark from '../../assets/logo-white.png'
+import search_light from '../../assets/search-w.png'
+import search_dark from '../../assets/search-b.png'
+import toogle_dark from '../../assets/day.png'
+import toogle_light from '../../assets/night.png'
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className='navbar'>
+      <img src={logo_light} alt='' className=''/>
+      <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>Features</li>
+          <li>About</li>
+      </ul>
+
+      <div className='searchbox'>
+        <input type='text' placeholder='search'></input>
+        <img src={search_dark} alt=''/>
+      </div>
+
+      <img src={toogle_dark} alt='' className='toggle-icon'/>
     </div>
   )
 }
